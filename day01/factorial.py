fea14985f0c6de4ca710c8d766da8a0c9d6fd203
kanggyunghun <kkh1574@inha.edu>
2024-02-08 10:51:32 +0900

    import mymath

--- day01/factorial.py
+++ day01/factorial.py
@@ -1,36 +1,8 @@
-# def factorial(number) -> int:
-#     """
-#     factorial by repetition
-#     :param number: number(int)
-#     :return: factorial result (int)
-#     """
-#     result = 1
-#     for i in range(1, number+1):
-#         result = result * i
-#     return result
-
-def factorial(number) -> int:
-    if number == 1:
-        return 1
-    else:
-        return number * factorial(number-1)
-
-
-def nCr(n, r) -> int:
-    """
-    조합 함수
-    :param n:
-    :param r:
-    :return:
-    """
-    numerator = factorial(n)
-    denominator = factorial(n-r) * factorial(r)
-
-    return numerator / denominator
+import mymath as mm
 
 if __name__ == '__main__': # 매직매서드
-    # n = int(input("Input n: "))
-    # r = int(input("Input r: "))
-    # print(f'{n}C{r} = {nCr(n,r)}')
-    f = int(input())
-    print(factorial(f))
+    n = int(input("Input n: "))
+    r = int(input("Input r: "))
+    print(f'{n}C{r} = {mm.nCr(n,r)}')
+    # f = int(input())
+    # print(factorial(f))
