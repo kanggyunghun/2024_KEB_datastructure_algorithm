cfc8ececd3fd3fa8401e11c4a41c2e97af73edff
kanggyunghun <kkh1574@inha.edu>
2024-02-08 10:41:28 +0900

    recursionn preview

--- day01/factorial.py
+++ day01/factorial.py
@@ -1,8 +1,19 @@
-def factorial(n) -> int:
-    if n == 1:
+# def factorial(number) -> int:
+#     """
+#     factorial by repetition
+#     :param number: number(int)
+#     :return: factorial result (int)
+#     """
+#     result = 1
+#     for i in range(1, number+1):
+#         result = result * i
+#     return result
+
+def factorial(number) -> int:
+    if number == 1:
         return 1
     else:
-        return n * factorial(n-1)
+        return number * factorial(number-1)
 
 
 def nCr(n, r) -> int:
@@ -18,6 +29,8 @@ def nCr(n, r) -> int:
     return numerator / denominator
 
 if __name__ == '__main__': # 매직매서드
-    n = int(input("Input n: "))
-    r = int(int("Input r: "))
-    print(f'{n}C{r} = {nCr(n,r)}')
+    # n = int(input("Input n: "))
+    # r = int(input("Input r: "))
+    # print(f'{n}C{r} = {nCr(n,r)}')
+    f = int(input())
+    print(factorial(f))
